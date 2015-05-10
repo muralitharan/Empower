44a614e87f60983178f879fbbbcd3160072b884d
Dharani Reddy <dharani.reddy@emids.com>
2015-05-10 15:28:16 +0530

    Demographics and confirm screen is designed.

--- Empower/AppointmentDetailsConformViewController.swift
+++ Empower/AppointmentDetailsConformViewController.swift
@@ -11,6 +11,13 @@ import UIKit
 
 class AppointmentDetailsConformViewController : UIViewController {
     
+    @IBOutlet private weak var physicianLabel: UILabel!
+    @IBOutlet private weak var locationLabel: UILabel!
+    @IBOutlet private weak var appointmentTypeLabel: UILabel!
+    @IBOutlet private weak var patientTypeLabel: UILabel!
+    @IBOutlet private weak var preferredDayAndTimeLabel: UILabel!
+    @IBOutlet private weak var preferredMethodOfContactLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.title = "Schedule Appointment"
